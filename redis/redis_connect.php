4a1925f394da8a6f136a2422cb0363a104ced681
Yin Jie <email_yin@163.com>
2015-08-28 21:59:59 +0800

    modify file redis_connect.php

--- redis/redis_connect.php
+++ redis/redis_connect.php
@@ -1,6 +1,7 @@
 <?php
     echo "I'm ready!<br>";
-    require __DIR__.'/predis/autoload.php';
+    require './predis/autoload.php';
+    //require __DIR__.'/predis/autoload.php'; // OK
     $redis = new Predis\Client(array(
         'scheme'=>'tcp',
         'host'=>'127.0.0.1',
