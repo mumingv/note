b0796f98b0c0b49164386c59d1133a4d55fb3fff
Yin Jie <email_yin@163.com>
2015-09-01 19:54:00 +0800

    daily commit

--- redis/redis_connect.php
+++ redis/redis_connect.php
@@ -8,5 +8,43 @@
         'port'=>5160,
     ));
 
-    echo $redis->get('foo');
-?>
+    // 页面访问计数 
+    if ($redis->exists('count') == 0) {
+        $redis->set('count', 0);
+    }
+    echo "Reflesh time: ";
+    $redis->incr('count');
+    echo $redis->get('count');
+    echo '<br>';
+
+    //$redis->lpush('list', '4', '5', '6');
+    //echo $redis->get('foo');
+    //$redis->set('zzz2', 'test');
+    /*
+    try {
+        echo $redis->get('list');
+    } catch (Exception $e) {
+        echo "Message: {$e->getMessage()}";
+    }
+    */
+
+    // PHP关联数组 vs Redis字符串MSET/MGET
+    $userName = array(
+        'user:1:name' => 'Tom',
+        'user:2:name' => 'Jack',
+    );
+    $redis->mset($userName);
+
+    $users = array_keys($userName);
+    print $users;
+    //print_r($redis->mget($users));
+    
+
+
+
+
+
+
+
+
+
