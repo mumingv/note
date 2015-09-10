1851d8d8dba2bc296aefa9df18051a53175fa250
Yin Jie <email_yin@163.com>
2015-09-10 11:01:58 +0800

    mod

---
+++ php/manual/03_lang_ref/00_basic/seperate_from_html2.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>hello.php</title>
+</head>
+<body>
+<?php $expression = 1; ?>
+<?php if ($expression == true): ?>
+  This will show if the expression is true.
+<?php else: ?>
+  Otherwise this will show.
+<?php endif; ?>
+</body>
+</html>
