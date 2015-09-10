794bad3a5461f5796f352be8e2d4984b2e6fdeec
Yin Jie <email_yin@163.com>
2015-09-10 13:11:31 +0800

    add

---
+++ php/manual/03_lang_ref/02_type/String1.php
@@ -0,0 +1,25 @@
+<?php
+// Key point:
+// 1. 单引号中只解析\'和\\这两个转义字符，其他均直接输出（包括$var也不解析)
+echo 'this is a simple string';
+
+// 可以录入多行
+echo 'You can also have embedded newlines in 
+strings this way as it is
+okay to do';
+
+// 输出： Arnold once said: "I'll be back"
+echo 'Arnold once said: "I\'ll be back"';
+
+// 输出： You deleted C:\*.*?
+echo 'You deleted C:\\*.*?';
+
+// 输出： You deleted C:\*.*?
+echo 'You deleted C:\*.*?';
+
+// 输出： This will not expand: \n a newline
+echo 'This will not expand: \n a newline';
+
+// 输出： Variables do not $expand $either
+echo 'Variables do not $expand $either';
+?> 
