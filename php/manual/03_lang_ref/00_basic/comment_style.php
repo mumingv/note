5665a7685e9d07c57bbb1a44e68855af18cd4852
Yin Jie <email_yin@163.com>
2015-09-10 11:52:20 +0800

    mod

---
+++ php/manual/03_lang_ref/00_basic/comment_style.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<p>This is a test</p>\n"; // This is a one-line c++ style comment
+    /* This is a multi line comment
+     * yet another line of comment */
+    echo "<p>This is yet another test</p>\n";
+    echo "<p>One Final Test</p>\n"; # This is a one-line shell-style comment
+?> 
---
+++ php/manual/03_lang_ref/00_basic/seperate_notation.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<p>This is a test</p>";
+?>
+
+<?php echo "<p>This is a test</p>" ?>
+
+<?php echo '<p>We omitted the last closing tag</p>';
